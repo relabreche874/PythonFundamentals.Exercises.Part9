9cbc7aca0fed779fe4e639831c7dfe1fbe8effc2
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-03-22 11:02:07 -0400

    Updated read_json code

---
+++ json_helper.py
@@ -0,0 +1,25 @@
+import json
+import os
+import pickle
+
+file_path = '/Users/robert/Desktop/PythonProjects/PyFun9/data/super_smash_bros/link.json'
+
+def read_json(file_path):
+    with open(file_path) as file:
+        data = json.load(file)
+        return data
+
+read_json(file_path)
+
+#
+# def read_all_json_files():
+#     json_list = []
+#     json_list.append(read_json(file_path))
+#
+#
+#
+# def write_pickle():
+#     pass
+#
+# def load_pickle():
+#     pass
